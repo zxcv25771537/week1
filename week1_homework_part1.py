9bd8aa7d796c607a130cb5277e6d0c13cf0ba006
zxcv25771537 <161662632+zxcv25771537@users.noreply.github.com>
2024-02-29 10:03:07 +0800

    Add files via upload
    week1_homework_part1

---
+++ week1_homework_part1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 09:28:44 2024
+
+@author: Student
+"""
+
+print("ax^2+bx+c=0")
+
+a = float(input("a="))
+b = float(input("b="))
+c = float(input("c="))
+
+print ("x1=")
+print ((-b + (b**2 - 4*a*c)**(0.5)) / (2*a))
+print ("x2=")
+print ((-b + (b**2 - 4*a*c)**(0.5)) / (2*a))
+
